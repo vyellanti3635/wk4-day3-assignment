e08163ea5de301d18ba76d105ccead4732247d75
Vivek Yellanti <vivekyellanti@viveks-MacBook-Pro.local>
2026-02-01 01:17:59 -0600

    assignment submission

--- routes/index.js
+++ routes/index.js
@@ -10,22 +10,34 @@ const basic = auth.basic({
   file: path.join(__dirname, '../users.htpasswd'),
 });
 
+// Home page route (Simple Kitchen main page)
 router.get('/', (req, res) => {
-  //res.send('It works!');
-  res.render('form', { title: 'Registration form' });
+  res.render('index', { title: 'Simple Kitchen' });
 });
 
-router.get('/registrations', basic.check((req, res) => {
+// Registration form route
+router.get('/register', (req, res) => {
+  res.render('form', { title: 'Register to subscribe' });
+});
+
+// Thank you page route
+router.get('/thankyou', (req, res) => {
+  res.render('thankyou', { title: 'Thank you for your registration!' });
+});
+
+// View registrations route (protected)
+router.get('/registrants', basic.check((req, res) => {
   Registration.find()
     .then((registrations) => {
-      res.render('index', { title: 'Listing registrations', registrations });
+      res.render('registrants', { title: 'Listing registrations', registrations });
     })
     .catch(() => { 
       res.send('Sorry! Something went wrong.'); 
     });
 }));
 
-router.post('/', 
+// Handle registration form submission
+router.post('/register', 
     [
         check('name')
         .isLength({ min: 1 })
@@ -35,19 +47,20 @@ router.post('/',
         .withMessage('Please enter an email'),
     ],
     (req, res) => {
-        //console.log(req.body);
         const errors = validationResult(req);
         if (errors.isEmpty()) {
           const registration = new Registration(req.body);
           registration.save()
-            .then(() => {res.send('Thank you for your registration!');})
+            .then(() => {
+              res.redirect('/thankyou');
+            })
             .catch((err) => {
               console.log(err);
               res.send('Sorry! Something went wrong.');
             });
           } else {
             res.render('form', { 
-                title: 'Registration form',
+                title: 'Register to subscribe',
                 errors: errors.array(),
                 data: req.body,
              });
